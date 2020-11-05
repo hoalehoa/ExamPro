910758255e39a465cc5c7ebf4937f0eae23e36ca
Hoa <wi19b046@technikum-wien.at>
2020-11-05 10:35:20 +0100

    Add project files.

---
+++ ExamPro/SpecFlowFeature1.feature
@@ -0,0 +1,36 @@
+﻿Feature: Trigonometrische Funktionen
+	In order to calculate the trigonometric functions
+	As a math idiot
+	I want to be told the sine, cosine and tangen
+
+
+Scenario Outline: Calculate sine 
+	Given the angle alpha is <phi>
+	When the sin function is pressed
+	Then the result for sine should be <sin_phi>
+
+Examples: 
+| phi      | sin_phi |
+| 0.0      | 0.0     |
+| 3.141597 | 0.0     |
+
+Scenario Outline: Calculate cosine 
+	Given the angle alpha is <phi>
+	When the cosin function is pressed
+	Then the result for sine should be <cos_phi>
+
+Examples: 
+| phi      | cos_phi |
+| 0.0      | 1.0     |
+| 3.141597 | -1.0     |
+
+Scenario Outline: Calculate tan
+	Given the angle alpha is <phi>
+	When the tan function is pressed
+	Then the result for sine should be <tan_phi>
+
+Examples: 
+| phi      | tan_phi |
+| 0.0      | 0.0     |
+| 3.141597 | 0.0     |
+
---
+++ ExamPro/TrigonometrischeFunktionenSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace ExamPro
+{
+    [Binding]
+    public class TrigonometrischeFunktionenSteps
+    {
+        Decimal phi;
+        Decimal result;
+
+        [Given(@"the angle alpha is (.*)")]
+        public void GivenTheAngleAlphaIs(Decimal p0)
+        {
+            phi = p0;
+        }
+
+        [When(@"the sin function is pressed")]
+        public void WhenTheSinFunctionIsPressed()
+        {
+            result = (Decimal)Math.Sin((double)phi);
+        }
+
+        [Then(@"the result for sine should be (.*)")]
+        public void ThenTheResultForSineShouldBe(Decimal p0)
+        {
+            Assert.Equal(p0, result, 5);
+        }
+
+        [When(@"the cosin function is pressed")]
+        public void WhenTheCosinFunctionIsPressed()
+        {
+            result = (Decimal)Math.Cos((double)phi);
+        }
+
+        [When(@"the tan function is pressed")]
+        public void WhenTheTanFunctionIsPressed()
+        {
+            result = (Decimal)Math.Tan((double)phi);
+        }
+
+    }
+}
